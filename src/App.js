f0d8782d6ea5e0363a23e3b9ab4c69707563d016
SeeniselvamB <bseeniselvam2006@gmail.com>
2025-07-02 22:02:23 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,3 +1,56 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Navbar from './Navbar';
+// import Home from './Home';
+// import About from './About';
+// import Timing from './Timing';
+// import Events from './Branch';
+// import Images from './Images';
+// import Location from './Location';
+// import GalleryPage from './GalleryPage';
+// import HistoryPage from './HistoryPage';
+// import TamilHistory from './TamilHistory';
+// import Media from './Media';
+// import BranchDetail from './BranchDetail';
+// // import Father from './Father';
+// // import FatherHistory from './FatherHistory';
+
+// import './App.css';
+
+// function App() {
+  
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Navbar />
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <>
+//                 <section id="home"><Home /></section>
+//                 <section id="about"><About /></section>
+//                 {/* <section id="father"><Father /></section> */}
+//                 <section id="timing"><Timing /></section>
+//                 <section id="gallery"><Images /></section>
+//                 <section id="events"><Events /></section>
+//                 <section id="media"><Media /></section>
+//                 <section id="location"><Location /></section>
+//               </>
+//             }
+//           />
+//           <Route path="/gallery" element={<GalleryPage />} />
+//           <Route path="/history" element={<HistoryPage />} />
+//           <Route path="/history/tamil" element={<TamilHistory />} />
+//           <Route path="/branch/:id" element={<BranchDetail />} />
+//           {/* <Route path="/fathers/history" element={<FatherHistory />} /> */}
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
@@ -14,31 +67,15 @@ import Media from './Media';
 import BranchDetail from './BranchDetail';
 // import Father from './Father';
 // import FatherHistory from './FatherHistory';
-
 import './App.css';
 
 function App() {
-  
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navbar />
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <section id="home"><Home /></section>
-                <section id="about"><About /></section>
-                {/* <section id="father"><Father /></section> */}
-                <section id="timing"><Timing /></section>
-                <section id="gallery"><Images /></section>
-                <section id="events"><Events /></section>
-                <section id="media"><Media /></section>
-                <section id="location"><Location /></section>
-              </>
-            }
-          />
+          <Route path="/" element={<HomeLayout />} />
           <Route path="/gallery" element={<GalleryPage />} />
           <Route path="/history" element={<HistoryPage />} />
           <Route path="/history/tamil" element={<TamilHistory />} />
@@ -47,6 +84,21 @@ function App() {
         </Routes>
       </div>
     </Router>
+  );
+}
+
+function HomeLayout() {
+  return (
+    <>
+      <section id="home"><Home /></section>
+      <section id="about"><About /></section>
+      {/* <section id="father"><Father /></section> */}
+      <section id="timing"><Timing /></section>
+      <section id="gallery"><Images /></section>
+      <section id="events"><Events /></section>
+      <section id="media"><Media /></section>
+      <section id="location"><Location /></section>
+    </>
   );
 }
 
