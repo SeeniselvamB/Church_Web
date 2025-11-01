01ec8ea5e1de68d6d10524a510b5707f7ac8bc1b
SeeniselvamB <bseeniselvam2006@gmail.com>
2025-11-01 16:53:42 +0530

    Updates

--- src/components/About.js
+++ src/components/About.js
@@ -16,10 +16,7 @@ function About() {
       <div className="about-container glass-card" data-aos="fade-up">
         <h2 data-aos="fade-down" data-aos-delay="100">About the Church</h2>
         <p data-aos="fade-up" data-aos-delay="300">
-          Our church stands as a beacon of faith and love, guided by the teachings of Jesus Christ. 
-          Rooted in a history of devotion and service, we honor the commitment and sacrifices made by early believers 
-          who spread the light of Christianity to the world. 
-          Together, we continue their mission — growing in faith, compassion, and unity.
+          The CSI IMMANUEL Church Chithumoondradaippu traces its origins to the American Ceylon Mission, which began evangelistic work in Tamil Nadu in 1834 under Rev. Levi Spaulding. In 1846, Rev. James Herrick continued this mission in the Thirumangalam region, extending its reach to Mallanginar and Chithumoondradaippu. In 1857, Rev. Herrick, along with Rev. A. Perumal, initiated Christian ministry in Chithumoondradaippu, leading to the establishment of a new church in 1859. From its modest beginnings, the church has grown into a strong and faithful community, built on enduring values of faith, service, and unity.
         </p>
         <Link
           to="/history"
